bd0232e25d3cdf7ec8808838f3fe3fd1970f706c
Portallos <114810485+Portallos@users.noreply.github.com>
2022-10-01 16:05:17 +0300

    Add files via upload
    Lectia 1 in c++ de pe data de 01/10/2022

---
+++ main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+
+using namespace std;
+
+// int g;
+
+int main()
+{
+    //int i, a, c;
+    //int x,X;
+    //x=4;
+    //X=5;
+    
+    //cout<<"x este de fapt, intradevar, in realitate="<<x;
+    //cout<<endl<<"Deci, in concluzie X="<<X;
+    
+    //unsigned int e = 4; 
+    
+    //double d = 4.5;
+    //int dd=1;
+    
+    //cout<<d=dd;
+    
+    //char f = "e" ;
+    //cout << f;
+    
+    //bool h=true;
+    //bool r=false;
+    //cout<<h<<"  "<<r;
+  /*  
+    cout<<"marimea int in memorie este de:"<<sizeof(int)<<"bytes"<<endl;
+    cout<<"marimea double in memorie este de:"<<sizeof(double)<<"bytes"<<endl;   
+    cout<<"marimea float in memorie este de:"<<sizeof(float)<<"bytes"<<endl;
+    cout<<"marimea long in memorie este de:"<<sizeof(long)<<"bytes"<<endl;
+    cout<<"marimea bool in memorie este de:"<<sizeof(bool)<<"bytes"<<endl;   
+    cout<<"marimea unsigned int in memorie este de:"<<sizeof(unsigned int)<<"bytes"<<endl;  
+   */ 
+   
+   char ccc ='A';
+   int nr =(int)ccc+32;
+   cout <<(char)nr;
+/*    cout<<"Scrieti un numar:";
+    
+    cin>>i;
+    
+    cout<<i;
+    
+    for(i=1;i<5;i++)
+    {
+        if(i==2)
+        {
+            i++;
+        }
+    }
+
+*/
+    return 0;
+}
+/*
+int cauta_nume()
+{
+   cout<<g;
+}
+*/
+
+
+
