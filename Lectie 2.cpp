5e71476d19970b3f9a6608c6a4f862fc6617de0f
Portallos <114810485+Portallos@users.noreply.github.com>
2022-10-08 16:10:10 +0300

    Add files via upload
    Lectia 1 si 2

---
+++ Lectie 2.cpp
@@ -0,0 +1,148 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    //Ex:1----------------------------------------------------------------------
+    
+    /*
+    //pow(a, b) inseamna "a" la puterea "b"
+    
+    int a,b;
+    cout<<"a= ";
+    cin>>a;
+    cout<<"b= ";
+    cin>>b;
+    cout<<a<<" la puterea "<<b<< "este egal cu "<<pow(a,b)<<endl;
+    return 0;
+    */
+    
+    //Ex:2----------------------------------------------------------------------
+    
+    /*
+    //sqrt(a) inseamna radicalul lui "a"
+    
+    int a;
+
+    cout<<"a= ";
+    cin>>a;
+    cout<<"radacina patrata a lui "<<a<<" este: "<<sqrt(a)<<endl;
+    */
+    
+    //Ex:3----------------------------------------------------------------------
+    
+    /*
+    //cbrt(a) inseamna radacina cubica lui "a"
+    
+    int a;
+    
+    cout<<"a= ";
+    cin>>a;
+    cout<<"radacina cubica a lui "<<a<<"este "<<cbrt(a)<<endl;
+    */
+    
+    //Ex:4----------------------------------------------------------------------
+    
+    /*
+    //round(a) il va rotunji pe la cel mai apropiat nr intreg "a"
+    float a;
+    
+    cout<<"a= ";
+    cin>>a;
+    cout<<"a rotunjit la cel mai apropiat nr intreg este "<<round(a)<<endl;
+    */
+    
+    //Ex:5----------------------------------------------------------------------
+    
+    /*
+    //trunc(a) ii va elimina toate zecimalele lui "a"
+    
+    float a;
+    
+    cout<<"a= ";
+    cin>>a;
+    cout<<"a fara zecimale este "<<trunc(a)<<endl;
+    */
+    
+    //Ex:6----------------------------------------------------------------------
+    
+    /*
+    //abs(a) va da modulul lui a
+    //sin, cos, tan
+    
+    int a;
+    
+    cout<<"a= ";
+    cin>>a;
+    cout<<"sin lui a este"<<sin(a)<<endl;
+    cout<<"cos lui a este"<<cos(a)<<endl;
+    cout<<"tan lui a este"<<tan(a)<<endl;
+    */
+    
+    //Ex:7----------------------------------------------------------------------
+    
+    /*
+    //operatori unari
+    int x = -5;
+    if(a == b) {
+        cout<<"Ok";
+    }
+    */
+    
+    //Ex:7----------------------------------------------------------------------
+    
+    //Operatori Aritmetici: +(plus), -(minus), *(inmultire), /(impartire), %(restul unei impartiri)
+    
+    /*
+    int a, b;
+    
+    cout<<"a= ";
+    cin>>a;
+    
+    cout<<"b= ";
+    cin>>b;
+    cout<<"a plus b fara zecimale este "<<trunc(a+b)<<endl;
+    cout<<"a minus b fara zecimale este "<<trunc(a-b)<<endl;
+    cout<<"a inmultit cu b fara zecimale este "<<trunc(a*b)<<endl;
+    cout<<"a impartit la b fara zecimale este "<<trunc(a/b)<<endl;
+    cout<<"restul lui a impartit la b fara zecimale este "<<trunc(a%b)<<endl;
+    */
+    
+    //Ex:8----------------------------------------------------------------------
+    
+    /*
+    int a,b;
+    cout<<"a= ";
+    cin>>a;
+    cout<<"b= ";
+    cin>>b;
+    cout<<a<<" impartit la "<<b<<" este egal cu "<<a/b<<" rest "<<a%b<<endl;
+    */
+    
+    //Ex:9----------------------------------------------------------------------
+    
+    /*
+    //Operatori Relationali: <(mai mic), >(mai mare), <=(mai mic sau egal), >=(mai mare sau egal), ==(egal), !=(nu este egal)
+    
+    int a,b;
+    cout<<"a= ";
+    cin>>a;
+    cout<<"b= ";
+    cin>>b;
+    
+    if(a != b){
+        cout << "a si b nu sunt egale";
+    }
+    
+    if(a == b){
+        cout << "a si b sunt egale";
+    }
+    */
+    
+    //Ex:10----------------------------------------------------------------------
+    
+   //Operatori Logici: //, &&, !
+}
+
