5fe44838fa7fbed3a4b0d89a9d6a3e0dc395e2ee
Portallos <114810485+Portallos@users.noreply.github.com>
2022-10-15 16:14:39 +0300

    Add files via upload

---
+++ Lectie 3.cpp
@@ -0,0 +1,162 @@
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    //Ex:1----------------------------------------------------------------------
+    
+    /*
+    
+    //Operatori de atribuire complecsi: +=, -=, *=, /=, %=
+    
+    int a, b;
+    
+    cout<<"a= ";
+    cin>>a;
+    cout<<"b= ";
+    cin>>b;
+
+    cout<<" "<<endl;
+    cout<<"a plus b "<<(a+b)<<endl;
+    cout<<"a minus b "<<(a-b)<<endl;
+    cout<<"a inmultit cu b "<<(a*b)<<endl;    
+    cout<<"a impartit cu b "<<(a/b)<<endl;    
+    cout<<"restul impartirii lui a la b "<<(a%b)<<endl;    
+    
+    */
+    
+    //Ex:2----------------------------------------------------------------------
+    
+    //Operatori de incrementare / decrementare: ++ / --
+    
+    /*
+    
+    int a, b;
+    
+    cout<<"a= ";
+    cin>>a;
+    
+    cout<<" "<<endl;
+    cout<<"a plus 1= "<<(a=++)<<endl;
+    cout<<"a minus 1=  "<<(a=--)<<endl;
+
+    */
+    
+    //Ex:3----------------------------------------------------------------------
+    
+    //Operatori conditional ? => expresie ? Da : Nu
+    
+    /*
+    
+    int a;
+    
+    cout<<"a= ";
+    cin>>a;
+    cout<<"a este "<<(a%2 == 0 ? "par" : "impar");
+    
+    */
+    
+    //Ex:4----------------------------------------------------------------------
+    
+    /*
+    interschimbarea a 2 variabile
+    
+    */
+    
+    //Ex:5----------------------------------------------------------------------
+    
+    /*
+    
+    int a;
+    
+    cout<<"a= ";
+    cin>>a;
+
+    if(a%2)
+    {
+        cout<<"a este impar";
+    }
+    else
+    {
+        cout<<"a este par";
+    }
+    
+    */
+    
+    //Ex:6----------------------------------------------------------------------
+    
+    /*
+    
+    int a, b;
+    
+    cout<<"a= ";
+    cin>>a;
+    cout<<"b= ";
+    cin>>b;
+    
+    if(a>b)
+    {
+        cout<<"a este mai mare decat b";
+    }
+    
+    else
+    {
+    
+    if(a<b)
+    {
+        cout<<"a este mai mic ca b";
+    }
+    
+    else
+    
+    {
+        cout<<"a este egal cu b";
+    }
+    }
+    
+    */
+    
+    //Ex:6----------------------------------------------------------------------
+    
+    /*
+    
+    int a, b, c;
+    
+    cout<<"a= ";
+    cin>>a;
+    cout<<"b= ";
+    cin>>b;
+    cout<<"c= ";
+    cin>>c;
+    
+    if(a > b)
+    {
+        if(c > b)  
+        {
+            cout<<"c este cel mai mare";
+        }
+        else
+        {
+            cout<<"a este cel mai mare";
+        }
+    }
+    else
+    {
+        if(c > b)  
+        {
+            cout<<"c este cel mai mare";
+        }
+        else
+        {
+            cout<<"b este cel mai mare";
+        }
+    }
+    
+    */
+    
+    
+    return 0;
+}
