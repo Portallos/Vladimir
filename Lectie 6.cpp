cc7f7eff58ef3d01e7eadddad7eaedea074ac695
Portallos <114810485+Portallos@users.noreply.github.com>
2022-11-05 15:59:46 +0200

    Add files via upload

---
+++ Lectie 6.cpp
@@ -0,0 +1,128 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+/*
+
+    int n;
+    cout << "n = ";
+    cin >> n;
+    
+    for(int i=1; i<=n; i++)
+    {
+        
+        for(int j=1; j<=i; j++)
+        {
+            cout<<"*";
+            
+        }
+        
+        cout<<endl;
+        
+    }
+    
+    */
+    
+    //Ex:1----------------------------------------------------------------------
+
+    /*
+    
+int n;
+    cout << "n = ";
+    cin >> n;
+    
+    for(int i=1; i<=n; i++)
+    {
+        
+        for(int j=1; j<=i; j++)
+        {
+           cout << j << " " ;
+        }
+        
+        cout<<endl;
+        
+    }
+    
+    */
+    
+    //Ex:2----------------------------------------------------------------------
+
+    /*
+    
+    int n;
+    int g=0;
+
+    cout << "n = ";
+    cin >> n;
+    
+    for(int i=1; i<=n; i++)
+    {
+        
+        for(int j=1; j<=i; j++)
+        {   
+            cout << g << " " ;
+            g++;
+        
+        }
+        
+        cout<<endl;
+        
+    }
+    
+    */
+    
+    //Ex:3----------------------------------------------------------------------
+    
+    /*
+    
+    string n;
+    cin>> n;
+    
+    while(0<1)
+    {
+    
+    if(0<1)
+    {
+    cout<< n << endl;
+    }
+    
+    }
+    */
+    
+    //Ex:4----------------------------------------------------------------------
+    
+    int a, b, c, d, e;
+    
+    void functie()
+    {
+    if(a!=0)
+    }
+    if(b!=0)
+    {
+    if(c!=0)
+    {
+    if(d!=0)
+    {
+    if(e!=0)
+    {
+    cout<<"FINAL!";
+    }
+    cout<<"S-a blocat la D";
+    }
+    cout<<"S-a blocat la C";
+    }
+    cout<<"S-a blocat la B";
+    }
+    cout<<"S-a blocat la A";
+    }
+    }
+    
+    
+    
+    return 0;
+    
+}
