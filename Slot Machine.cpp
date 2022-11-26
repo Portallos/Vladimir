fa91a0c1dcbe6a3ea11f32cd4a74ba75edd20934
Portallos <114810485+Portallos@users.noreply.github.com>
2022-11-26 15:52:33 +0200

    Add files via upload

---
+++ Slot Machine.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hello World";
+
+    return 0;
+}
