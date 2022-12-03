e2070ea9c29d772abe74dc254354d7625ecc96c6
Portallos <114810485+Portallos@users.noreply.github.com>
2022-12-03 15:50:43 +0200

    Add files via upload

---
+++ Lectie 10.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+int main()
+{
+
+/*
+    
+int prim = 0;
+int n;
+cout <<"Numar = ";
+cin >> n;
+
+int i;
+
+    for(i=2 ; i <= n/2 ; i++)
+    {
+       if((n%i)==0)
+       {
+           prim++;
+       }
+    }
+    
+    if (prim==0)
+    {
+        cout <<"Numarul este prim";
+    }
+    
+    else
+    {
+        cout <<"Numarul nu este prim";
+    }
+   
+*/  
+
+    //Ex:1----------------------------------------------------------------------
+
+
+/*
+    
+int n;
+cout <<"Numar = ";
+cin >> n;
+
+int i;
+
+    for(i=2 ; i <= n/2 ; i++)
+    {
+       if((n%i)==0)
+       {
+          cout<< "Nu este prim";
+          
+          i=n;
+       }
+       
+       else
+       {
+           if(i==n/2)
+           {
+                cout<< "Este prim"<< endl;
+           }
+
+       }
+       
+    }
+    
+    
+*/
+
+
+   return 0; 
+}
