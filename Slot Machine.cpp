48b46f08a2ab26c16ca96eb2c6b01c968a576b71
Portallos <114810485+Portallos@users.noreply.github.com>
2022-12-09 19:16:16 +0200

    Update Slot Machine.cpp

--- Slot Machine.cpp
+++ Slot Machine.cpp
@@ -39,7 +39,7 @@ int main()
     
     cout << endl;
     
-    
+
     cout << "   ";
     cout << " | ";
     cout << a;
@@ -61,6 +61,77 @@ int main()
     cout << "_|_";
     cout << "_";
     cout << "_| ";
+    
+    
+    
+    for(a=0; b=0; c=0)
+    {
+        cout << endl << endl <<"Jackpot!";
+        break;
+    }
+
+    for(a=1; b=1; c=1)
+    {
+        cout << endl << endl <<"Jackpot!";
+        break;
+    }
+
+    for(a=2; b=2; c=2)
+    {
+        cout << endl << endl <<"Jackpot!";
+        break;
+    }
+    
+    for(a=3; b=3; c=3)
+    {
+        cout << endl << endl <<"Jackpot!";
+        break;
+    }
+    
+    for(a=4; b=4; c=4)
+    {
+        cout << endl << endl <<"Jackpot!";
+        break;
+    }
+    
+    for(a=5; b=5; c=5)
+    {
+        cout << endl << endl <<"Jackpot!";
+        break;
+    }
+    
+    for(a=5; b=5; c=5)
+    {
+        cout << endl << endl <<"Jackpot!";
+        break;
+    }
+    
+    for(a=6; b=6; c=6)
+    {
+        cout << endl << endl <<"Jackpot!";
+        break;
+    }
+    
+    for(a=7; b=7; c=7)
+    {
+        cout << endl << endl <<"Jackpot!";
+        break;
+    }
+    
+    for(a=8; b=8; c=8)
+    {
+        cout << endl << endl <<"Jackpot!";
+        break;
+    }
+    
+    for(a=9; b=9; c=9)
+    {
+        cout << endl << endl <<"Jackpot!";
+        break;
+    }
+    
+    
+    
     
     
     
