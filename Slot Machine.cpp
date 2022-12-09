cdfde10c487da4aab313dacd3d8b1e227bbea548
Portallos <114810485+Portallos@users.noreply.github.com>
2022-12-09 17:18:38 +0200

    Update Slot Machine.cpp

--- Slot Machine.cpp
+++ Slot Machine.cpp
@@ -1,14 +1,69 @@
 #include <iostream>
 #include <cmath>
-#include <string>
-#include <stdlib.h>
-#include <time.h>
-
+#include <ctime>
+#include <cstdlib>
 using namespace std;
 
 int main()
 {
-    cout<<"Hello World";
+    int a, b, c;
+    srand(time(0));
+    a = (rand() % 9 );
+    b = (rand() % 9 );
+    c = (rand() % 9 );
 
+    
+
+    cout << "   ";
+    cout << "  _";
+    cout << "_";
+    cout << "_ _";
+    cout << "_";
+    cout << "_ _";
+    cout << "_";
+    cout << "_  ";
+    
+    
+    cout<< endl;
+    
+    
+    cout << "   ";
+    cout << " | ";
+    cout << " ";
+    cout << " | ";
+    cout << " ";
+    cout << " | ";
+    cout << " ";
+    cout << " | ";
+    
+    
+    cout << endl;
+    
+    
+    cout << "   ";
+    cout << " | ";
+    cout << a;
+    cout << " | ";
+    cout << b;
+    cout << " | ";
+    cout << c;
+    cout << " | ";
+    
+    
+    cout << endl;
+    
+    
+    cout << "   ";
+    cout << " |_";
+    cout << "_";
+    cout << "_|_";
+    cout << "_";
+    cout << "_|_";
+    cout << "_";
+    cout << "_| ";
+    
+    
+    
+    
     return 0;
 }
