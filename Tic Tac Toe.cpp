b57d8dbeded8ca0c7bad21f5a0ceaf2b8a812d29
Portallos <114810485+Portallos@users.noreply.github.com>
2023-01-07 16:02:34 +0200

    eyjuj

--- Tic Tac Toe.cpp
+++ Tic Tac Toe.cpp
@@ -4,9 +4,14 @@ using namespace std;
 
 int main()
 {   
+    
+    int times = 0;
+
     int num;
     int mum;
-    int times = 0;
+    
+    int n1=0, n2=0, n3=0, n4=0, n5=0, n6=0, n7=0, n8=0, n9=0;
+    
     char a = 'A'; 
     char b = 'B';
     char c = 'C';
@@ -18,99 +23,119 @@ int main()
     char i = 'I';
     cout<< endl << endl;
 
-    while(times<=8)
+ while(times<=8)
 {
-
-    cout<<"      "<< a <<" | "<< b <<" | "<< c <<" "<< endl;
-    cout<<"     ---|---|---"<< endl;
-    cout<<"      "<< d <<" | "<< e <<" | "<< f <<" "<< endl;
-    cout<<"     ---|---|---"<< endl;
-    cout<<"      "<< g <<" | "<< h <<" | "<< i <<" "<< endl;
-    
     cin >> num;
     
     if(num==1)
     {
-        a=='X';
+        a='X';
+        n1++;
     }
     if(num==2)
     {
-        b=='X';
+        b='X';
+        n2++;
     }
     if(num==3)
     {
-        c=='X';
+        c='X';
+        
     }
     if(num==4)
     {
-        d=='X';
+        d='X';
+        n1++;
     }
     if(num==5)
     {
-        e=='X';
+        e='X';
+        n5++;
     }
     if(num==6)
     {
         f=='X';
+        n6++;
     }
     if(num==7)
     {
-        g=='X';
+        g='X';
+        n7++;
     }
     if(num==8)
     {
-        h=='X';
+        h='X';
+        n8++;
     }
     if(num==9)
     {
-        i=='X';
+        i='X';
+        n9++;
     }
     
     cout<<"      "<< a <<" | "<< b <<" | "<< c <<" "<< endl;
     cout<<"     ---|---|---"<< endl;
     cout<<"      "<< d <<" | "<< e <<" | "<< f <<" "<< endl;
     cout<<"     ---|---|---"<< endl;
-    cout<<"      "<< g <<" | "<< h <<" | "<< i <<" "<< endl;
+    cout<<"      "<< g <<" | "<< h <<" | "<< i <<" "<< endl << endl;
+    
+
     
     cin >> mum;
     if(mum==1)
     {
-        a=='O';
+        a='O';
+        n1++;
     }
     if(mum==2)
     {
-        b=='O';
+        b='O';
+        n2++;
     }
     if(mum==3)
     {
-        c=='O';
+        c='O';
+        n3++;
     }
     if(mum==4)
     {
-        d=='O';
+        d='O';
+        n4++;
     }
     if(mum==5)
     {
-        e=='O';
+        e='O';
+        n5++;
     }
     if(mum==6)
     {
-        f=='O';
+        f='O';
+        n6++;
     }
     if(mum==7)
     {
-        g=='O';
+        g='O';
+        n7++;
     }
     if(mum==8)
     {
-        h=='O';
+        h='O';
+        n8++;
     }
     if(mum==9)
     {
-        i=='O';
+        i='O';
+        n9++;
     }
     
+    cout<<"      "<< a <<" | "<< b <<" | "<< c <<" "<< endl;
+    cout<<"     ---|---|---"<< endl;
+    cout<<"      "<< d <<" | "<< e <<" | "<< f <<" "<< endl;
+    cout<<"     ---|---|---"<< endl;
+    cout<<"      "<< g <<" | "<< h <<" | "<< i <<" "<< endl << endl;
+    
     cout << endl;
+    if (n1=)
     
     times++;
 }
