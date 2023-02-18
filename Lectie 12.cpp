08b2aa0dc88db2ad97971bde3053b02b613562cf
Portallos <114810485+Portallos@users.noreply.github.com>
2023-02-18 14:20:04 +0200

    Update Lectie 12.cpp

--- Lectie 12.cpp
+++ Lectie 12.cpp
@@ -3,271 +3,60 @@
 using namespace std;
 
 int main()
-{   
-    
-    int times = 0;
-
-    int num;
-    int mum;
-    
-    int n1=0, n2=0, n3=0, n4=0, n5=0, n6=0, n7=0, n8=0, n9=0;
-    
-    char a = 'A'; 
-    char b = 'B';
-    char c = 'C';
-    char d = 'D';
-    char e = 'E';
-    char f = 'F';
-    char g = 'G';
-    char h = 'H';
-    char i = 'I';
-    cout<< endl << endl;
-
- while(times<=8)
 {
-    cout<<"      "<< a <<" | "<< b <<" | "<< c <<" "<< endl;
-    cout<<"     ---|---|---"<< endl;
-    cout<<"      "<< d <<" | "<< e <<" | "<< f <<" "<< endl;
-    cout<<"     ---|---|---"<< endl;
-    cout<<"      "<< g <<" | "<< h <<" | "<< i <<" "<< endl << endl;
+    
+    /*
+    
+    int a[100], n;
+    cin >> n;
     
     
-    cin >> num;
-    
-    if(num==1)
+    for(int i=0; i<n; i++)
     {
-    cout << endl;
-    if (n1==0)
+        cout << "a["<< i << "]=";
+        
+        cin >> a[i];
+        
+    
+    }
+    
+    int n1=0;
+    
+        for(int i=0; i<n; i++)
     {
-        a='X';
-        n1++;
+        cout  << a[i] <<" ";
+        
     }
-    }
+    */
 
+    //Ex:1----------------------------------------------------------------------
+    
+    /*
+    
+    int a[100], n;
+    cin >> n;
+    
+    
+    for(int i=0; i<n; i++)
+    {
+        cout << "a["<< i << "]=";
+        
+        cin >> a[i];
+    }
+    
+    int n1=0;
+    
+        for(int i=0; i<n; i++)
+    {
+        
+        n1+=a[i];
+    }
+    
+    cout << n1;
+    
+    */
+    
+    //Ex:2----------------------------------------------------------------------
 
-    if(num==2)
-    {
-    cout << endl;
-    if (n2==0)
-    {
-        b='X';
-        n2++;
-    }
-    }
-    
-    
-    if(num==1)
-    {
-    cout << endl;
-    if (n1==0)
-    {
-        a='X';
-        n1++;
-    }
-    }
-    cout << endl;
-    if (n2=0)
-    {
-        if(num==2)
-    {
-        b='X';
-        n2++;
-    }
-    }
-    
-    
-    cout << endl;
-    if (n3=0)
-    {
-        if(num==3)
-    {
-        c='X';
-        n3++;
-    }
-    }
-    
-    
-    cout << endl;
-    if (n4=0)
-    {
-        if(num==4)
-    {
-        d='X';
-        n4++;
-    }
-    }
-    
-    cout << endl;
-    if (n5=0)
-    {
-        if(num==5)
-    {
-        e='X';
-        n5++;
-    }
-    }
-    
-    cout << endl;
-    if (n6=0)
-    {
-        if(num==6)
-    {
-        f=='X';
-        n6++;
-    }
-    }
-    
-    cout << endl;
-    if (n7=0)
-    {
-        if(num==7)
-    {
-        g='X';
-        n7++;
-    }
-    }
-    
-    
-    cout << endl;
-    if (n8=0)
-    {
-        if(num==8)
-    {
-        h='X';
-        n8++;
-    }
-    }
-    
-    
-    cout << endl;
-    if (n9=0)
-    {
-        if(num==9)
-    {
-        i='X';
-        n9++;
-    }
-    }
-    
-    
-    cout<<"      "<< a <<" | "<< b <<" | "<< c <<" "<< endl;
-    cout<<"     ---|---|---"<< endl;
-    cout<<"      "<< d <<" | "<< e <<" | "<< f <<" "<< endl;
-    cout<<"     ---|---|---"<< endl;
-    cout<<"      "<< g <<" | "<< h <<" | "<< i <<" "<< endl << endl;
-    
-
-    
-    cin >> num;
-    
-    
-    cout << endl;
-    if (n1=0)
-    {
-        if(mum==1)
-    {
-        a='O';
-        n1++;
-    }
-    }
-    
-    
-    cout << endl;
-    if (n2=0)
-    {
-        if(mum==2)
-    {
-        b='O';
-        n2++;
-    }
-    }
-    
-    
-    cout << endl;
-    if (n3=0)
-    {
-        if(mum==3)
-    {
-        c='O';
-        n3++;
-    }
-    }
-    
-    
-    cout << endl;
-    if (n4=0)
-    {
-        if(mum==4)
-    {
-        d='O';
-        n4++;
-    }
-    }
-    
-    cout << endl;
-    if (n5=0)
-    {
-        if(mum==5)
-    {
-        e='O';
-        n5++;
-    }
-    }
-    
-    cout << endl;
-    if (n6=0)
-    {
-        if(mum==6)
-    {
-        f=='O';
-        n6++;
-    }
-    }
-    
-    cout << endl;
-    if (n7=0)
-    {
-        if(mum==7)
-    {
-        g='O';
-        n7++;
-    }
-    }
-    
-    
-    cout << endl;
-    if (n8=0)
-    {
-        if(mum==8)
-    {
-        h='O';
-        n8++;
-    }
-    }
-    
-    
-    cout << endl;
-    if (n9=0)
-    {
-        if(mum==9)
-    {
-        i='O';
-        n9++;
-    }
-    }
-    
-    cout<<"      "<< a <<" | "<< b <<" | "<< c <<" "<< endl;
-    cout<<"     ---|---|---"<< endl;
-    cout<<"      "<< d <<" | "<< e <<" | "<< f <<" "<< endl;
-    cout<<"     ---|---|---"<< endl;
-    cout<<"      "<< g <<" | "<< h <<" | "<< i <<" "<< endl << endl;
-    
-    
-    
-    times++;
-}
-    
     return 0;
-    
-    
 }
