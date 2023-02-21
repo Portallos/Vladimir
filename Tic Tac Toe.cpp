f34abcfb5207d6f36b93b68d0d86b7bc00b6cca1
Portallos <114810485+Portallos@users.noreply.github.com>
2023-02-21 23:06:51 +0200

    Update Tic Tac Toe.cpp

--- Tic Tac Toe.cpp
+++ Tic Tac Toe.cpp
@@ -1,273 +1,645 @@
 #include <iostream>
 
-using namespace std;
+ using namespace std;
 
-int main()
-{   
-    
-    int times = 0;
-
-    int num;
-    int mum;
-    
-    int n1=0, n2=0, n3=0, n4=0, n5=0, n6=0, n7=0, n8=0, n9=0;
-    
-    char a = 'A'; 
-    char b = 'B';
-    char c = 'C';
-    char d = 'D';
-    char e = 'E';
-    char f = 'F';
-    char g = 'G';
-    char h = 'H';
-    char i = 'I';
-    cout<< endl << endl;
-
- while(times<=8)
+ 
+int
+main () 
 {
-    cout<<"      "<< a <<" | "<< b <<" | "<< c <<" "<< endl;
-    cout<<"     ---|---|---"<< endl;
-    cout<<"      "<< d <<" | "<< e <<" | "<< f <<" "<< endl;
-    cout<<"     ---|---|---"<< endl;
-    cout<<"      "<< g <<" | "<< h <<" | "<< i <<" "<< endl << endl;
+  
+ 
+int times = 0;
+  
+ 
+int num;
+  
+int mum;
+  
+ 
+int n1 = 0, n2 = 0, n3 = 0, n4 = 0, n5 = 0, n6 = 0, n7 = 0, n8 = 0, n9 = 0;
+  
+ 
+char a = 'A';
+  
+char b = 'B';
+  
+char c = 'C';
+  
+char d = 'D';
+  
+char e = 'E';
+  
+char f = 'F';
+  
+char g = 'G';
+  
+char h = 'H';
+  
+char i = 'I';
+  
+cout << endl << endl;
+  
+ 
+while (times <= 8)
     
-    
-    cin >> num;
-    
-    if(num==1)
     {
-    cout << endl;
-    if (n1==0)
-    {
-        a='X';
-        n1++;
-    }
-    }
-
-
-    if(num==2)
-    {
-    cout << endl;
-    if (n2==0)
-    {
-        b='X';
-        n2++;
-    }
-    }
-    
-    
-    if(num==1)
-    {
-    cout << endl;
-    if (n1==0)
-    {
-        a='X';
-        n1++;
-    }
-    }
-    cout << endl;
-    if (n2=0)
-    {
-        if(num==2)
-    {
-        b='X';
-        n2++;
-    }
-    }
-    
-    
-    cout << endl;
-    if (n3=0)
-    {
-        if(num==3)
-    {
-        c='X';
-        n3++;
-    }
-    }
-    
-    
-    cout << endl;
-    if (n4=0)
-    {
-        if(num==4)
-    {
-        d='X';
-        n4++;
-    }
-    }
-    
-    cout << endl;
-    if (n5=0)
-    {
-        if(num==5)
-    {
-        e='X';
-        n5++;
-    }
-    }
-    
-    cout << endl;
-    if (n6=0)
-    {
-        if(num==6)
-    {
-        f=='X';
-        n6++;
-    }
-    }
-    
-    cout << endl;
-    if (n7=0)
-    {
-        if(num==7)
-    {
-        g='X';
-        n7++;
-    }
-    }
-    
-    
-    cout << endl;
-    if (n8=0)
-    {
-        if(num==8)
-    {
-        h='X';
-        n8++;
-    }
-    }
-    
-    
-    cout << endl;
-    if (n9=0)
-    {
-        if(num==9)
-    {
-        i='X';
-        n9++;
-    }
-    }
-    
-    
-    cout<<"      "<< a <<" | "<< b <<" | "<< c <<" "<< endl;
-    cout<<"     ---|---|---"<< endl;
-    cout<<"      "<< d <<" | "<< e <<" | "<< f <<" "<< endl;
-    cout<<"     ---|---|---"<< endl;
-    cout<<"      "<< g <<" | "<< h <<" | "<< i <<" "<< endl << endl;
-    
-
-    
-    cin >> num;
-    
-    
-    cout << endl;
-    if (n1=0)
-    {
-        if(mum==1)
-    {
-        a='O';
-        n1++;
-    }
-    }
-    
-    
-    cout << endl;
-    if (n2=0)
-    {
-        if(mum==2)
-    {
-        b='O';
-        n2++;
-    }
-    }
-    
-    
-    cout << endl;
-    if (n3=0)
-    {
-        if(mum==3)
-    {
-        c='O';
-        n3++;
-    }
-    }
-    
-    
-    cout << endl;
-    if (n4=0)
-    {
-        if(mum==4)
-    {
-        d='O';
-        n4++;
-    }
-    }
-    
-    cout << endl;
-    if (n5=0)
-    {
-        if(mum==5)
-    {
-        e='O';
-        n5++;
-    }
-    }
-    
-    cout << endl;
-    if (n6=0)
-    {
-        if(mum==6)
-    {
-        f=='O';
-        n6++;
-    }
-    }
-    
-    cout << endl;
-    if (n7=0)
-    {
-        if(mum==7)
-    {
-        g='O';
-        n7++;
-    }
-    }
-    
-    
-    cout << endl;
-    if (n8=0)
-    {
-        if(mum==8)
-    {
-        h='O';
-        n8++;
-    }
-    }
-    
-    
-    cout << endl;
-    if (n9=0)
-    {
-        if(mum==9)
-    {
-        i='O';
-        n9++;
-    }
-    }
-    
-    cout<<"      "<< a <<" | "<< b <<" | "<< c <<" "<< endl;
-    cout<<"     ---|---|---"<< endl;
-    cout<<"      "<< d <<" | "<< e <<" | "<< f <<" "<< endl;
-    cout<<"     ---|---|---"<< endl;
-    cout<<"      "<< g <<" | "<< h <<" | "<< i <<" "<< endl << endl;
-    
-    
-    
-    times++;
+      
+cout << "      " << a << " | " << b << " | " << c << " " << endl;
+      
+cout << "     ---|---|---" << endl;
+      
+cout << "      " << d << " | " << e << " | " << f << " " << endl;
+      
+cout << "     ---|---|---" << endl;
+      
+cout << "      " << g << " | " << h << " | " << i << " " << endl << endl;
+      
+ 
+ 
+cin >> num;
+      
+ 
+if (num == 1)
+	
+	{
+	  
+cout << endl;
+	  
+if (n1 == 0)
+	    
+	    {
+	      
+a = 'X';
+	      
+n1++;
+	    
 }
+	
+}
+      
+ 
+ 
+if (num == 2)
+	
+	{
+	  
+cout << endl;
+	  
+if (n2 == 0)
+	    
+	    {
+	      
+b = 'X';
+	      
+n2++;
+	    
+}
+	
+}
+      
+ 
+ 
+if (num == 3)
+	
+	{
+	  
+cout << endl;
+	  
+if (n3 == 0)
+	    
+	    {
+	      
+c = 'X';
+	      
+n3++;
+	    
+}
+	
+}
+      
+if (num == 4)
+	
+	{
+	  
+cout << endl;
+	  
+if (n4 == 0)
+	    
+	    {
+	      
+d = 'X';
+	      
+n4++;
+	    
+}
+	
+}
+      
+ 
+ 
+if (num == 5)
+	
+	{
+	  
+cout << endl;
+	  
+if (n5 == 0)
+	    
+	    {
+	      
+e = 'X';
+	      
+n5++;
+	    
+}
+	
+}
+      
+ 
+ 
+if (num == 6)
+	
+	{
+	  
+cout << endl;
+	  
+if (n6 == 0)
+	    
+	    {
+	      
+f = 'X';
+	      
+n6++;
+	    
+}
+	
+}
+      
+ 
+if (num == 7)
+	
+	{
+	  
+cout << endl;
+	  
+if (n7 == 0)
+	    
+	    {
+	      
+g = 'X';
+	      
+n7++;
+	    
+}
+	
+}
+      
+ 
+if (num == 8)
+	
+	{
+	  
+cout << endl;
+	  
+if (n8 == 0)
+	    
+	    {
+	      
+h = 'X';
+	      
+n8++;
+	    
+}
+	
+}
+      
+ 
+if (num == 9)
+	
+	{
+	  
+cout << endl;
+	  
+if (n9 == 0)
+	    
+	    {
+	      
+i = 'X';
+	      
+n9++;
+	    
+}
+	
+}
+
+
+ 
+if(a=='X')
+{
+    if(b=='X')
+    {
+        if(c=='X')
+        {
+            cout<<"1st player wins!";
+            return 0;
+        }
+    }
+}
+  
+if(a=='X')
+{
+    if(b=='X')
+    {
+        if(c=='X')
+        {
+            cout<<"1st player wins!";
+            return 0;
+        }
+    }
+}
+ 
+if(g=='X')
+{
+    if(h=='X')
+    {
+        if(i=='X')
+        {
+            cout<<"1st player wins!";
+            return 0;
+        }
+    }
+}
+ 
+if(a=='X')
+ {
+    if(d=='X')
+    {
+        if(g=='X')
+        {
+            cout<<"1st player wins!";
+            return 0;
+        }
+    }
+}
+
+if(b=='X')
+ {
+    if(e=='X')
+    {
+        if(h=='X')
+        {
+            cout<<"1st player wins!";
+            return 0;
+        }
+    }
+}
+
+if(c=='X')
+ {
+    if(f=='X')
+    {
+        if(i=='X')
+        {
+            cout<<"1st player wins!";
+            return 0;
+        }
+    }
+}
+
+
+if(c=='X')
+ {
+    if(e=='X')
+    {
+        if(g=='X')
+        {
+            cout<<"1st player wins!";
+            return 0;
+        }
+    }
+}
+
+if(a=='X')
+ {
+    if(e=='X')
+    {
+        if(i=='X')
+        {
+            cout<<"1st player wins!";
+            return 0;
+        }
+    }
+}
+
+
+
+cout << "      " << a << " | " << b << " | " << c << " " << endl;
+      
+cout << "     ---|---|---" << endl;
+      
+cout << "      " << d << " | " << e << " | " << f << " " << endl;
+      
+cout << "     ---|---|---" << endl;
+      
+cout << "      " << g << " | " << h << " | " << i << " " << endl << endl;
+      
+ 
+ 
+ 
+cin >> mum;
+      
+ 
+ 
+if (mum == 1)
+	
+	{
+	  
+cout << endl;
+	  
+if (n1 == 0)
+	    
+	    {
+	      
+a = 'O';
+	      
+n1++;
+	    
+}
+	
+}
+      
+ 
+ 
+if (mum == 2)
+	
+	{
+	  
+cout << endl;
+	  
+if (n2 == 0)
+	    
+	    {
+	      
+b = 'O';
+	      
+n2++;
+	    
+}
+	
+}
+      
+ 
+ 
+if (mum == 3)
+	
+	{
+	  
+cout << endl;
+	  
+if (n3 == 0)
+	    
+	    {
+	      
+c = 'O';
+	      
+n3++;
+	    
+}
+	
+}
+      
+if (mum == 4)
+	
+	{
+	  
+cout << endl;
+	  
+if (n4 == 0)
+	    
+	    {
+	      
+d = 'O';
+	      
+n4++;
+	    
+}
+	
+}
+      
+ 
+ 
+if (mum == 5)
+	
+	{
+	  
+cout << endl;
+	  
+if (n5 == 0)
+	    
+	    {
+	      
+e = 'O';
+	      
+n5++;
+	    
+}
+	
+}
+      
+ 
+ 
+if (mum == 6)
+	
+	{
+	  
+cout << endl;
+	  
+if (n6 == 0)
+	    
+	    {
+	      
+f = 'O';
+	      
+n6++;
+	    
+}
+	
+}
+      
+ 
+if (mum == 7)
+	
+	{
+	  
+cout << endl;
+	  
+if (n7 == 0)
+	    
+	    {
+	      
+g = 'O';
+	      
+n7++;
+	    
+}
+	
+}
+      
+ 
+if (mum == 8)
+	
+	{
+	  
+cout << endl;
+	  
+if (n8 == 0)
+	    
+	    {
+	      
+h = 'O';
+	      
+n8++;
+	    
+}
+	
+}
+      
+ 
+if (mum == 9)
+	
+	{
+	  
+cout << endl;
+	  
+if (n9 == 0)
+	    
+	    {
+	      
+i = 'O';
+	      
+n9++;
+	    
+}
+	
+}
+      
+ 
+ 
+cout << "      " << a << " | " << b << " | " << c << " " << endl;
+      
+cout << "     ---|---|---" << endl;
+      
+cout << "      " << d << " | " << e << " | " << f << " " << endl;
+      
+cout << "     ---|---|---" << endl;
+      
+cout << "      " << g << " | " << h << " | " << i << " " << endl << endl;
+      
+ 
+ 
+if(a=='O')
+{
+    if(b=='O')
+    {
+        if(c=='O')
+        {
+            cout<<"2nd player wins!";
+            return 0;
+        }
+    }
+}
+  
+if(a=='O')
+{
+    if(b=='O')
+    {
+        if(c=='O')
+        {
+            cout<<"2nd player wins!";
+            return 0;
+        }
+    }
+}
+ 
+if(g=='O')
+{
+    if(h=='O')
+    {
+        if(i=='O')
+        {
+            cout<<"2nd player wins!";
+            return 0;
+        }
+    }
+}
+ 
+if(a=='O')
+ {
+    if(d=='O')
+    {
+        if(g=='O')
+        {
+            cout<<"2nd player wins!";
+            return 0;
+        }
+    }
+}
+
+if(b=='O')
+ {
+    if(e=='O')
+    {
+        if(h=='O')
+        {
+            cout<<"2nd player wins!";
+            return 0;
+        }
+    }
+}
+
+if(c=='O')
+ {
+    if(f=='O')
+    {
+        if(i=='O')
+        {
+            cout<<"2nd player wins!";
+            return 0;
+        }
+    }
+}
+
+if(c=='O')
+ {
+    if(e=='O')
+    {
+        if(g=='O')
+        {
+            cout<<"2nd player wins!";
+            return 0;
+        }
+    }
+}
+
+if(a=='O')
+ {
+    if(e=='O')
+    {
+        if(i=='O')
+        {
+            cout<<"2nd player wins!";
+            return 0;
+        }
+    }
+}
+
+times++;
     
-    return 0;
-    
-    
+}
+  
+ 
+return 0;
+
+ 
+ 
 }
