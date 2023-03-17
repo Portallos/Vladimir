b5049418c67104ba2e92070dcfd549fef0fb13cf
Portallos <114810485+Portallos@users.noreply.github.com>
2023-03-17 17:24:14 +0200

    Add files via upload

---
+++ Scrima!.cpp
@@ -0,0 +1,23 @@
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
+
+
+
+#include <iostream>
+#include <cstdlib>
+
+
+using namespace std;
+
+int main()
+{
+    
+    
+    cout<<"emanuel"<< endl ;
+        sleep(1);
+    cout<< endl <<"emanuel";
+    return 0;
+}
