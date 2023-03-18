feac67cfc0d3dd7e822f6cdd4ee43a300149842d
Portallos <114810485+Portallos@users.noreply.github.com>
2023-03-18 14:38:00 +0200

    Update Scrima!.cpp

--- Scrima!.cpp
+++ Scrima!.cpp
@@ -1,23 +1,29 @@
-#ifdef _WIN32
-#include <Windows.h>
-#else
-#include <unistd.h>
-#endif
-
-
-
 #include <iostream>
 #include <cstdlib>
-
+#include <unistd.h>
 
 using namespace std;
 
 int main()
 {
     
+    int n;
+    cout<< endl << endl << endl << endl << endl << endl << endl <<"S";
+    usleep(500000);
+    cout<< endl << endl << endl << endl << endl << endl << endl <<"Sc";
+    usleep(500000);
+    cout<< endl << endl << endl << endl << endl << endl << endl <<"Scr";
+    usleep(500000);
+    cout<< endl << endl << endl << endl << endl << endl << endl <<"Scri";
+    usleep(500000);
+    cout<< endl << endl << endl << endl << endl << endl << endl <<"Scrim";
+    usleep(500000);
+    cout<< endl << endl << endl << endl << endl << endl << endl <<"Scrima";
+    usleep(500000);
+    cout<< endl << endl << endl << endl << endl << endl << endl <<"Scrima!";
     
-    cout<<"emanuel"<< endl ;
-        sleep(1);
-    cout<< endl <<"emanuel";
+    cout<< endl << endl << endl << endl << endl << endl << endl;;
+    
+    cin>>n;
     return 0;
 }
