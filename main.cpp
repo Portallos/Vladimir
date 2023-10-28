6bc78ac864902a23ecceed433767664a26f4d82b
Portallos <114810485+Portallos@users.noreply.github.com>
2023-10-28 15:48:24 +0300

    intro

---
+++ main.cpp
@@ -0,0 +1,151 @@
+#include <iostream> //default
+
+#include <thread> //ignore cin
+
+#include <cstdlib> //wait
+#include <unistd.h>
+
+#include <ctime> //random
+
+#include <chrono> //sleep_for
+
+#include <vector> //piste
+
+using namespace std;
+
+int main()
+{
+int linewait=0;
+
+//intro
+
+
+while(linewait<22)
+{
+cout<<endl;
+
+linewait++;
+}
+
+linewait = 0;
+
+
+usleep (1500000);
+
+cout<< " ___ "<<endl;
+cout<< "/ __|"<<endl;
+cout<< "\\__ \\"<<endl;
+cout<< "|___/"<<endl;
+
+
+while(linewait<20)
+{
+cout<<endl;
+
+linewait++;
+}
+
+linewait = 0;
+
+
+usleep (750000);
+
+cout<< " ___    ___ "<<endl;
+cout<< "/ __|  / __|"<<endl;
+cout<< "\\__ \\ | (__"<<endl;
+cout<< "|___/  \\___|"<<endl;
+
+
+while(linewait<20)
+{
+cout<<endl;
+
+linewait++;
+}
+
+linewait = 0;
+
+
+usleep (750000);
+
+cout<< " ___    ___   ____"<<endl;
+cout<< "/ __|  / __| |  __|"<<endl;
+cout<< "\\__ \\ | (__  | |"<<endl;
+cout<< "|___/  \\___| |_|"<<endl;
+
+
+while(linewait<20)
+{
+cout<<endl;
+
+linewait++;
+}
+
+linewait = 0;
+
+
+usleep (750000);
+
+cout<< "                     _"<<endl;
+cout<< "                    (_)"<<endl;
+cout<< " ___    ___   ____   _"<<endl;
+cout<< "/ __|  / __| |  __| | |"<<endl;
+cout<< "\\__ \\ | (__  | |    | |"<<endl;
+cout<< "|___/  \\___| |_|    |_|"<<endl;
+
+
+while(linewait<20)
+{
+cout<<endl;
+
+linewait++;
+}
+
+linewait = 0;
+
+
+usleep (750000);
+
+cout<< "                     _"<<endl;
+cout<< "                    (_)"<<endl;
+cout<< " ___    ___   ____   _   ________"<<endl;
+cout<< "/ __|  / __| |  __| | | |  _   _ \\"<<endl;
+cout<< "\\__ \\ | (__  | |    | | | | | | | |"<<endl;
+cout<< "|___/  \\___| |_|    |_| |_| |_| |_|"<<endl;
+
+while(linewait<20)
+{
+cout<<endl;
+
+linewait++;
+}
+
+linewait = 0;
+
+
+usleep (750000);
+
+cout<< "                     _                       _ "<<endl;
+cout<< "                    (_)                     | |"<<endl;
+cout<< " ___    ___   ____   _   ________     ____  | |"<<endl;
+cout<< "/ __|  / __| |  __| | | |  _   _ \\   / _  | | |"<<endl;
+cout<< "\\__ \\ | (__  | |    | | | | | | | | | (_| | |_|"<<endl;
+cout<< "|___/  \\___| |_|    |_| |_| |_| |_|  \\__,_| (_)"<<endl;
+
+
+while(linewait<20)
+{
+cout<<endl;
+
+linewait++;
+}
+
+linewait = 0;
+
+
+cout<< "nigger";
+sleep (20000);
+
+
+return 0;
+}
